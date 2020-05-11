56aab2fea88f424745f417e1c7e94af6b5d2c253
LAPTOP-4O0R2UIR\Jonathan <jonathan@ssagroup.com>
2020-05-11 11:00:25 +0800

    Modify AR files

---
+++ public/frames/ar/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "823469.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': true,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 24464232, 24464162, 24464171, 24464199, 24464200, 24464175, 24464190, 24464169, 24464265, 24464179, 24464183, 24474414, 29033980, 29436173, 29437632, 29469428, 29496731, 29987892, 30325273, 30325276, 30328772 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/29822759/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/29822757/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/29822758/1/ammo.js', 'preload' : true},
+];
