8dc932fe11f83d4300820212e2ab2b3c58b6488c
Jonathan <52232027+Jonathan751997@users.noreply.github.com>
2020-05-14 17:34:35 +0800

    Modify AR files from marker-based to markerless (#10)

--- public/frames/ar/__settings__.js
+++ public/frames/ar/__settings__.js
@@ -1,13 +1,13 @@
 ASSET_PREFIX = "";
 SCRIPT_PREFIX = "";
-SCENE_PATH = "823469.json";
+SCENE_PATH = "919178.json";
 CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': true,
     'preserveDrawingBuffer': true,
     'preferWebGl2': true
 };
-SCRIPTS = [ 24464232, 24464162, 24464171, 24464199, 24464200, 24464175, 24464190, 24464169, 24464265, 24464179, 24464183, 24474414, 29033980, 29436173, 29437632, 29469428, 29496731, 29987892, 30325273, 30325276, 30328772 ];
+SCRIPTS = [ 30752496, 30752629, 30752638, 30752634, 30752564, 30752544, 30752632, 30752630, 30752633, 30752553, 30752631, 30752554, 30752635, 30752539, 30752565, 30752538, 30752637, 30752582, 30752562, 30752644, 30752457, 30768490 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -17,5 +17,5 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/29822759/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/29822757/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/29822758/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/30752627/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/30752567/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/30752477/1/ammo.js', 'preload' : true},
 ];
